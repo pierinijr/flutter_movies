833397afcece1f5bf356a78fe51e9f720e8efba8
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-27 18:52:30 -0300

    Add provider

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_movies/core/routes.dart';
 import 'package:flutter_movies/languages/generated/app_localizations.dart';
+import 'package:flutter_movies/view_model/movie_view_model.dart';
+import 'package:provider/provider.dart';
 
 class FlutterMovies extends StatelessWidget {
   const FlutterMovies({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Movies',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+          ChangeNotifierProvider(create: (_) => MovieViewModel()),
+        ],
+      child: MaterialApp(
+        title: 'Flutter Movies',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: "/home",
+        routes: routes,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
       ),
-      debugShowCheckedModeBanner: false,
-      initialRoute: "/home",
-      routes: routes,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
     );
   }
 }
