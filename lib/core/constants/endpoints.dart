bb612ad0f2aa6b308bc3af1074a48f794904dcd4
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-28 14:35:27 -0300

    Add constants file

---
+++ lib/core/constants/endpoints.dart
@@ -0,0 +1,4 @@
+class ConstantsEndpoints {
+  String get movieSearch => "https://api.themoviedb.org/3/movie/now_playing";
+  String get movieDetails => "https://api.themoviedb.org/3/movie/";
+}
---
+++ lib/core/constants/status_code.dart
@@ -0,0 +1,7 @@
+class ConstantsStatusCode {
+  int get success => 200;
+  int get userInvalidResponse => 100;
+  int get noInternet => 101;
+  int get invalidFormat => 102;
+  int get unknownError => 103;
+}
