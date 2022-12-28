ac25a716baadb6fea883f9a32308c0f3f0011442
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-28 14:46:18 -0300

    Update splash screen

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_movies/view/screens/view_home.dart';
+import 'package:flutter_movies/view/screens/view_splash.dart';
 
 final Map<String, WidgetBuilder> routes = {
+  "/": (context) => const ViewSplash(),
   "/home": (context) => const ViewHome(),
   // "/favorites": (context) => const ViewFavorites(),
 };
