2bb9760a72f132ff39fc4f875fb50c688b07961f
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-29 13:09:51 -0300

    Add colors

---
+++ lib/themes/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xFF1C1C27);
+  static const secondaryColor = Color.fromARGB(255, 40, 40, 56);
+  static const tertiaryColor = Color(0xFFF27706);
+
+  static const labelPrimaryColor = Color(0xFFEFEEF4);
+  static const labelSecondaryColor = Color(0xFF7B7B86);
+
+  static const ratingBar = Color(0xFFFFC107);
+  
+  static const primaryShimmer = Color(0xFF33337A);
+  static const secondaryShimmer = Color(0xFFB4B4FA);
+}
