bbde7847e65f3a63f67c33a223d1c793433c52a2
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-29 18:39:14 -0300

    Screen stacking update

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
 class Utils {
-  static void goView(
-    BuildContext context,
-    String name,
-    {Object? arguments}
-  ) {
-    Navigator.pushNamedAndRemoveUntil(
-      context,
-      name,
-      ModalRoute.withName('/home'),
-      arguments: arguments
-    );
+  static void goView(BuildContext context, String name,
+      {Object? arguments, bool removeUntil = false}) {
+    if (name.startsWith("/home")) {
+      removeUntil = true;
+    }
+
+    if (!removeUntil) {
+      Navigator.pushNamed(context, name, arguments: arguments);
+    } else {
+      Navigator.pushNamedAndRemoveUntil(context, name, (route) => false,
+          arguments: arguments);
+    }
   }
 }
