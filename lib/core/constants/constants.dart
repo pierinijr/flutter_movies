3114ce0e997aa5151a068334122351892b42bd79
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-29 13:09:27 -0300

    Update constants

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,9 +1,13 @@
 import 'package:flutter_movies/core/constants/endpoints.dart';
 import 'package:flutter_movies/core/constants/environment.dart';
+import 'package:flutter_movies/core/constants/images.dart';
+import 'package:flutter_movies/core/constants/spacings.dart';
 import 'package:flutter_movies/core/constants/status_code.dart';
 
 class Constants {
   static ConstantsEnvironment get environment => ConstantsEnvironment();
   static ConstantsEndpoints get endpoints => ConstantsEndpoints();
   static ConstantsStatusCode get statusCode => ConstantsStatusCode();
+  static ConstantsImages get images => ConstantsImages();
+  static ConstantsSpacings get spacings => ConstantsSpacings();
 }
---
+++ lib/core/constants/spacings.dart
@@ -0,0 +1,5 @@
+class ConstantsSpacings {
+  double get spacing2 => 2;
+  double get spacing8 => 8;
+  double get spacing16 => 16;
+}
