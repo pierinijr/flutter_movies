f5e73f25bcd0c21cd978041be32e8ced7f918339
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-29 13:08:54 -0300

    Add labels

---
+++ lib/view/widgets/labels/label_h3.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_movies/themes/colors.dart';
+import 'package:flutter_movies/view/widgets/labels/label_base.dart';
+
+class LabelH3 extends LabelBase {
+  const LabelH3({
+    Key? key,
+    required label,
+    overflow = TextOverflow.ellipsis,
+    maxLines = 1,
+    fontWeightType = FontWeight.normal,
+    color = AppColors.labelPrimaryColor,
+    height = 1.5,
+    textAlign = TextAlign.start
+  }) : super(
+            key: key,
+            label: label,
+            overflow: overflow,
+            maxLines: maxLines,
+            fontSize: 18,
+            height: height,
+            fontWeightType: fontWeightType,
+            color: color,
+            textAlign: textAlign
+            );
+}
