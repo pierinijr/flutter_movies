b26ae3175e28f0cc7018778d117ce89ee5cbd73a
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-29 22:45:40 -0300

    Add function getTimeString

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -14,4 +14,10 @@ class Utils {
           arguments: arguments);
     }
   }
+
+  static String getTimeString(int value) {
+    final int hour = value ~/ 60;
+    final int minutes = value % 60;
+    return '${hour.toString().padLeft(2, "0")}:${minutes.toString().padLeft(2, "0")}';
+  }
 }
