129001747c7149812e1d73e5779ae2f9a2acffbb
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-29 22:44:29 -0300

    Add original image endpoint

--- lib/core/constants/endpoints.dart
+++ lib/core/constants/endpoints.dart
@@ -2,4 +2,5 @@ class ConstantsEndpoints {
   String get movieSearch => "https://api.themoviedb.org/3/movie/now_playing";
   String get movieDetails => "https://api.themoviedb.org/3/movie/";
   String get moviePoster => "https://image.tmdb.org/t/p/w200";
+  String get movieBackdrop => "https://image.tmdb.org/t/p/original";
 }
