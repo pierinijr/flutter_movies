1d0f16eec1a13d36f964854cae1e70875a9d0ef8
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-30 14:01:57 -0300

    Update name details view model

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_movies/core/routes.dart';
 import 'package:flutter_movies/languages/generated/app_localizations.dart';
-import 'package:flutter_movies/view_model/detail_view_model.dart';
+import 'package:flutter_movies/view_model/details_view_model.dart';
 import 'package:flutter_movies/view_model/now_playing_view_model.dart';
 import 'package:provider/provider.dart';
 
--- lib/view/screens/view_detail/view_detail.dart
+++ lib/view/screens/view_detail/view_detail.dart
@@ -4,7 +4,7 @@ import 'package:flutter_movies/services/api_response.dart';
 import 'package:flutter_movies/view/screens/view_detail/view_detail_complete.dart';
 import 'package:flutter_movies/view/screens/view_detail/view_detail_error.dart';
 import 'package:flutter_movies/view/screens/view_detail/view_detail_loading.dart';
-import 'package:flutter_movies/view_model/detail_view_model.dart';
+import 'package:flutter_movies/view_model/details_view_model.dart';
 import 'package:provider/provider.dart';
 
 class ViewDetail extends StatefulWidget {
