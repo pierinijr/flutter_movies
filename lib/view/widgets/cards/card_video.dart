c34728dff5d097e212fa66f64a368bde73419804
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-31 12:36:33 -0300

    Fix card video overflow

--- lib/view/widgets/cards/card_video.dart
+++ lib/view/widgets/cards/card_video.dart
@@ -39,27 +39,29 @@ class _CardVideoState extends State<CardVideo> {
                   color: AppColors.tertiaryColor,
                 ),
               ),
-              SizedBox(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    LabelH4(
-                      label: widget.videoDetails.name ?? "",
-                      maxLines: 2,
-                      height: 1.0,
-                      fontWeightType: FontWeight.bold,
-                    ),
-                    LabelH6(
-                      label: publishedAt,
-                      color: AppColors.labelSecondaryColor,
-                    ),
-                    LabelH6(
-                      label: widget.videoDetails.type,
-                      color: AppColors.tertiaryColor,
-                      fontWeightType: FontWeight.bold,
-                    ),
-                  ],
+              Expanded(
+                child: SizedBox(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      LabelH4(
+                        label: widget.videoDetails.name ?? "",
+                        maxLines: 2,
+                        height: 1.0,
+                        fontWeightType: FontWeight.bold,
+                      ),
+                      LabelH6(
+                        label: publishedAt,
+                        color: AppColors.labelSecondaryColor,
+                      ),
+                      LabelH6(
+                        label: widget.videoDetails.type,
+                        color: AppColors.tertiaryColor,
+                        fontWeightType: FontWeight.bold,
+                      ),
+                    ],
+                  ),
                 ),
               )
             ],
