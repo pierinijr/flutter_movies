91faa006e89eb88433403191ef0277780b6ed18d
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-30 23:55:38 -0300

    Add endpoint to videos

--- lib/core/constants/endpoints.dart
+++ lib/core/constants/endpoints.dart
@@ -3,4 +3,5 @@ class ConstantsEndpoints {
   String get movieDetails => "https://api.themoviedb.org/3/movie/";
   String get moviePoster => "https://image.tmdb.org/t/p/w200";
   String get movieBackdrop => "https://image.tmdb.org/t/p/original";
+  String get listVideoParameter => "&append_to_response=videos";
 }
