af574478ed0fe676cdb6ca0d8f8ab18a8c954712
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-31 17:13:54 -0300

    Update views to support multiple dynamic lists

--- lib/view/widgets/lists/card_list_landscape/card_list.dart
+++ lib/view/widgets/lists/card_list_landscape/card_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_movies/core/constants/constants.dart';
 import 'package:flutter_movies/services/api_response.dart';
 import 'package:flutter_movies/view/widgets/lists/card_list_landscape/card_list_completed.dart';
 import 'package:flutter_movies/view/widgets/lists/card_list_error.dart';
@@ -8,12 +9,10 @@ class CardListLandscape extends StatefulWidget {
   const CardListLandscape({
     super.key,
     required this.response,
-    required this.title,
     required this.moreButton,
   });
 
   final ApiResponse<dynamic> response;
-  final String title;
   final bool moreButton;
 
   @override
@@ -27,10 +26,21 @@ class _CardListLandscapeState extends State<CardListLandscape> {
       case Status.loading:
         return const CardListLandscapeLoading();
       case Status.completed:
-        return CardListLandscapeCompleted(
-            response: widget.response,
-            title: widget.title,
-            moreButton: widget.moreButton);
+        return ListView.builder(
+                  primary: false,
+                  shrinkWrap: true,
+                  itemCount: widget.response.data.response.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding:
+                          EdgeInsets.only(bottom: Constants.spacings.spacing16),
+                      child: CardListLandscapeCompleted(
+                        data: widget.response.data.response[index],
+                        moreButton: widget.moreButton)
+                    );
+                  },
+                );
+
       case Status.error:
         String error = widget.response.message ?? "-";
         return CardListError(error: error);
--- lib/view/widgets/lists/card_list_landscape/card_list_completed.dart
+++ lib/view/widgets/lists/card_list_landscape/card_list_completed.dart
@@ -3,7 +3,6 @@ import 'package:flutter_movies/core/constants/constants.dart';
 import 'package:flutter_movies/core/utils.dart';
 import 'package:flutter_movies/languages/generated/app_localizations.dart';
 import 'package:flutter_movies/model/list_arguments_model.dart';
-import 'package:flutter_movies/services/api_response.dart';
 import 'package:flutter_movies/view/widgets/buttons/button_text.dart';
 import 'package:flutter_movies/view/widgets/cards/card_movie_portrait/card_movie.dart';
 import 'package:flutter_movies/view/widgets/labels/label_h3.dart';
@@ -12,12 +11,10 @@ import 'package:flutter_movies/view/widgets/labels/label_h4.dart';
 class CardListLandscapeCompleted extends StatefulWidget {
   const CardListLandscapeCompleted(
       {super.key,
-      required this.response,
-      required this.title,
+      required this.data,
       required this.moreButton});
 
-  final ApiResponse<dynamic> response;
-  final String title;
+  final dynamic data;
   final bool moreButton;
 
   @override
@@ -29,9 +26,8 @@ class _CardListLandscapeCompletedState
     extends State<CardListLandscapeCompleted> {
   @override
   Widget build(BuildContext context) {
-    ApiResponse<dynamic> response = widget.response;
-    dynamic data = response.data != null ? (response.data.response ?? []) : [];
-    List<dynamic> movieList = data.results ?? [];
+    List<dynamic> movieList = widget.data.results ?? [];
+    String title = Utils.listTitle(context, widget.data.type);
 
     return Column(
       children: [
@@ -42,17 +38,19 @@ class _CardListLandscapeCompletedState
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               LabelH3(
-                label: widget.title,
+                label: title,
                 fontWeightType: FontWeight.bold,
               ),
               if (widget.moreButton &&
-                  widget.response.data.response.totalResults > 20)
+                  widget.data.totalResults > 20)
                 ButtonText(
                     label: AppLocalizations.of(context)!.more,
                     action: () {
                       Utils.goView(context, "/list",
                           arguments: ListArgumentsModel(
-                              title: widget.title, type: data.runtimeType));
+                            title: title, 
+                            type: widget.data.runtimeType,
+                            listType: widget.data.type));
                     }),
             ],
           ),
@@ -74,6 +72,7 @@ class _CardListLandscapeCompletedState
                   primary: false,
                   scrollDirection: Axis.horizontal,
                   shrinkWrap: true,
+                  physics: const BouncingScrollPhysics(),
                   itemCount: movieList.length >= 15 
                     ? 15 
                     : movieList.length,
